b99ac66f7b861f018bb86a6abb2f691d6b335531
mrueda020 <mrueda020@hotmail.com>
2020-11-09 16:05:24 -0600

    Created Row.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
-
+import Row from "./Row";
+import requests from "./request";
 function App() {
   return (
     <div className="App">
       <h1>Netflix Clone</h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      ></Row>
+      <Row title="Trending" fetchUrl={requests.fetchTrending}></Row>
+      <Row title="Top rated" fetchUrl={requests.fetchTopRated}></Row>
+      <Row title="Comedy movies" fetchUrl={requests.fetchComedyMovies}></Row>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}></Row>
+      <Row title="Romance movies" fetchUrl={requests.fetchRomanceMovies}></Row>
+      <Row title="Action movies" fetchUrl={requests.fetchActionMovies}></Row>
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import "./Row.css";
+const posterBaseUrl = "https://image.tmdb.org/t/p/original";
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      console.log(request);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => {
+          return (
+            <img
+              className="row__poster"
+              src={`${posterBaseUrl}${movie.poster_path}`}
+              alt={movie.name}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
