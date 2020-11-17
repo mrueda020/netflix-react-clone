0bec73d66d3234808b6c8ca194c4bcfab6f56609
mrueda020 <mrueda020@hotmail.com>
2020-11-16 18:26:46 -0600

    Added more categories

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import Row from "./Row";
 import requests from "./request";
 import Banner from "./Banner";
 import Navbar from "./Navbar";
+import { useEffect, useState } from "react";
+import axios from "./axios";
+
 function App() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      console.log(requests.fetchCategories);
+      const request = await axios.get(`${requests.fetchCategories}`);
+      const categories = request.data.genres;
+      setCategories(categories);
+      return request;
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <Navbar></Navbar>
@@ -15,10 +31,14 @@ function App() {
       ></Row>
       <Row title="Trending" fetchUrl={requests.fetchTrending}></Row>
       <Row title="Top rated" fetchUrl={requests.fetchTopRated}></Row>
-      <Row title="Comedy movies" fetchUrl={requests.fetchComedyMovies}></Row>
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}></Row>
-      <Row title="Romance movies" fetchUrl={requests.fetchRomanceMovies}></Row>
-      <Row title="Action movies" fetchUrl={requests.fetchActionMovies}></Row>
+      {categories.map((categorie) => {
+        return (
+          <Row
+            title={`${categorie.name}`}
+            fetchUrl={`${requests.fetchCategorie}${categorie.id}`}
+          ></Row>
+        );
+      })}
     </div>
   );
 }
